4491c57d0d3d077f8f5d71afb2af8e478a4f6200
Khalil Najjar <knajjars@gmail.com>
2021-05-30 09:08:37 +0200

    charts improvement

--- app/controllers/charts_controller.rb
+++ app/controllers/charts_controller.rb
@@ -2,15 +2,15 @@ class ChartsController < ApplicationController
   before_action :set_device
 
   def humidity_stats
-    render json: @device.humidity_stats.group_by_minute(:created_at).average(:value)
+    render json: @device.humidity_stats.chart_data
   end
 
   def light_stats
-    render json: @device.light_stats.group_by_minute(:created_at).average(:value)
+    render json: @device.light_stats.chart_data
   end
 
   def temperature_stats
-    render json: @device.temperature_stats.group_by_minute(:created_at).average(:value)
+    render json: @device.temperature_stats.chart_data
   end
 
   private
@@ -18,4 +18,5 @@ class ChartsController < ApplicationController
   def set_device
     @device = Device.find(params[:device_id])
   end
+
 end
--- app/models/device.rb
+++ app/models/device.rb
@@ -1,5 +1,9 @@
 class Device < ApplicationRecord
-  has_many :stats, class_name: :DeviceStats, dependent: :destroy
+  has_many :stats, class_name: :DeviceStats, dependent: :destroy do
+    def chart_data
+      where(created_at: (Time.now - 1.week)..Time.now).group_by_hour(:created_at).average(:value).chart_json
+    end
+  end
 
   validates_presence_of :alias, :name
 
@@ -20,4 +24,5 @@ class Device < ApplicationRecord
   def temperature_stats
     stats.temperature
   end
+
 end
