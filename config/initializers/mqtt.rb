0e04b082d578eebd3f1a204e08b59079ae93252b
Khalil Najjar <khn@alteos.com>
2021-06-03 09:13:24 +0200

    fix sidekiq

--- config/initializers/mqtt.rb
+++ config/initializers/mqtt.rb
@@ -11,15 +11,8 @@ Thread.new do
 
   client.get do |topic, value|
     device_alias, measurement = topic.split('/')
+    created_at = Time.now
 
-    begin
-      device = Device.find_or_create_by!(alias: device_alias) do |d|
-        d.alias = device_alias
-        d.name = device_alias
-      end
-      DeviceStats.create!(device: device, value: value.to_f, measurement: measurement, created_at: Time.now)
-    rescue StandardError => e
-      Rails.logger.error "Something went wrong: #{e.inspect}"
-    end
+    SaveDeviceStatsJob.perform_later(device_alias, measurement, value, created_at)
   end
 end
